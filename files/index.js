47bb1897c0179bf5ff6887333a76c49caff81a03
tyhyjnjmj <93179733+tyhyjnjmj@users.noreply.github.com>
2025-03-10 04:17:20 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -7,8 +7,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'chero5.niyuot123.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOWM0YWRlNDk0Mzc4N2M1ODgwM2YxMTliZjM4MDU1NmYiLCJ0IjoiZjY2MDk5ZDktYTAxOS00MDQ0LTk2MWItZDc5NTJmMGFhY2ZlIiwicyI6Ik5UbGpOMlUxTVdFdFpqa3pPUzAwTm1OaExUZ3lZek10TldZMU9UazRaV1JsT0dZMyJ9
-';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOWM0YWRlNDk0Mzc4N2M1ODgwM2YxMTliZjM4MDU1NmYiLCJ0IjoiZjY2MDk5ZDktYTAxOS00MDQ0LTk2MWItZDc5NTJmMGFhY2ZlIiwicyI6Ik5UbGpOMlUxTVdFdFpqa3pPUzAwTm1OaExUZ3lZek10TldZMU9UazRaV1JsT0dZMyJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
